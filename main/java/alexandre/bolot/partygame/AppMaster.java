fd522556ebb0e41d414186537e3f7d8d35889c73
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2017-09-26 14:39:39 +0200

    Adding JavaDoc

--- main/java/alexandre/bolot/partygame/AppMaster.java
+++ main/java/alexandre/bolot/partygame/AppMaster.java
@@ -1,21 +1,34 @@
 package alexandre.bolot.partygame;
 
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+
 /*................................................................................................................................
  . Copyright (c)
  .
  . The AppMaster	 Class was Coded by : Alexandre BOLOT
  .
- . Last Modified : 23/08/17 16:33
+ . Last Modified : 26/09/17 14:39
  .
  . Contact : bolotalex06@gmail.com
  ...............................................................................................................................*/
 
-import android.support.v7.app.AppCompatActivity;
-
-import java.util.ArrayList;
-
+/**
+ This class is meant to be used inbetween <code>AppCompatActivity</code> and the Activities.<br>
+ It will allow access to the players list and the current player's index.
+ */
 public class AppMaster extends AppCompatActivity
 {
-    public static ArrayList<Player> players = new ArrayList<>();
-    public static int playerIndex = 0;
+    /**
+     List of all players.<br>
+     Can be accessed from any Activity extending <code>AppMaster</code>
+     */
+    public static ArrayList<Player> players     = new ArrayList<>();
+    /**
+     Index of the current player.<br>
+     Used to cycle through all the players of [players]
+     */
+    public static int               playerIndex = 0;
 }
