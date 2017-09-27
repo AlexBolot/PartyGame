00e15c72ca6caa5ff4533365128ad0517e45dd90
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2017-09-27 10:38:33 +0200

    Adding JavaDoc and minor changes

--- main/java/alexandre/bolot/partygame/LaunchActivity.java
+++ main/java/alexandre/bolot/partygame/LaunchActivity.java
@@ -1,22 +1,34 @@
 package alexandre.bolot.partygame;
 
-/*................................................................................................................................
- . Copyright (c)
- .
- . The LaunchActivity	 Class was Coded by : Alexandre BOLOT
- .
- . Last Modified : 23/08/17 02:42
- .
- . Contact : bolotalex06@gmail.com
- ...............................................................................................................................*/
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+/*................................................................................................................................
+ . Copyright (c)
+ .
+ . The LaunchActivity	 Class was Coded by : Alexandre BOLOT
+ .
+ . Last Modified : 27/09/17 10:38
+ .
+ . Contact : bolotalex06@gmail.com
+ ...............................................................................................................................*/
+
+/**
+ This class is the Activity managing the main_view.xml.<br>
+ This activity only displays a "play" button.<br>
+ <br>
+ __ Class Dependency : AppMaster, PlayersActivity __
+ */
 public class LaunchActivity extends AppMaster
 {
+    /**
+     This is the initialization method, called on the Activity's launch.<br>
+     It will clear the players list and inits the button's listener.
+     
+     @param savedInstanceState No idea what that does...
+     */
     @Override
     protected void onCreate (Bundle savedInstanceState)
     {
@@ -24,11 +36,6 @@ public class LaunchActivity extends AppMaster
         setContentView(R.layout.main_view);
         
         players.clear();
-    
-        players.add(new Player("Alex", 0));
-        players.add(new Player("Romain", 0));
-        players.add(new Player("Marie", 1));
-        players.add(new Player("Coralie", 1));
         
         Button btnPlayers = (Button) findViewById(R.id.btnPlay);
         
